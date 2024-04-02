73aad75844f9dea1de9b48275006bb1209d284b2
David Braun <2096055+DBraun@users.noreply.github.com>
2024-04-01 20:14:01 -0400

    Use a makefile (#3)

--- thirdparty/libfaust/download_libfaust.sh
+++ thirdparty/libfaust/download_libfaust.sh
@@ -4,22 +4,28 @@ VERSION=2.69.3
 
 if [ "$(uname)" = "Darwin" ]; then
     echo "You are running macOS"
-    curl -L https://github.com/grame-cncm/faust/releases/download/$VERSION/Faust-$VERSION-arm64.dmg -o Faust-$VERSION-arm64.dmg
-    hdiutil attach Faust-$VERSION-arm64.dmg
-    mkdir -p "darwin-arm64/Release"
-    cp -R /Volumes/Faust-$VERSION/Faust-$VERSION/* darwin-arm64/Release/
-    hdiutil detach /Volumes/Faust-$VERSION/
-
-    curl -L https://github.com/grame-cncm/faust/releases/download/$VERSION/Faust-$VERSION-x64.dmg -o Faust-$VERSION-x64.dmg
-    hdiutil attach Faust-$VERSION-x64.dmg
-    mkdir -p "darwin-x64/Release"
-    cp -R /Volumes/Faust-$VERSION/Faust-$VERSION/* darwin-x64/Release/
-    hdiutil detach /Volumes/Faust-$VERSION/
+    if [ ! -f Faust-$VERSION-arm64.dmg ]; then
+        curl -L https://github.com/grame-cncm/faust/releases/download/$VERSION/Faust-$VERSION-arm64.dmg -o Faust-$VERSION-arm64.dmg
+        hdiutil attach Faust-$VERSION-arm64.dmg
+        mkdir -p "darwin-arm64/Release"
+        cp -R /Volumes/Faust-$VERSION/Faust-$VERSION/* darwin-arm64/Release/
+        hdiutil detach /Volumes/Faust-$VERSION/
+    fi
+    
+    if [ ! -f Faust-$VERSION-x64.dmg ]; then
+        curl -L https://github.com/grame-cncm/faust/releases/download/$VERSION/Faust-$VERSION-x64.dmg -o Faust-$VERSION-x64.dmg
+        hdiutil attach Faust-$VERSION-x64.dmg
+        mkdir -p "darwin-x64/Release"
+        cp -R /Volumes/Faust-$VERSION/Faust-$VERSION/* darwin-x64/Release/
+        hdiutil detach /Volumes/Faust-$VERSION/
+    fi
 elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
     echo "You are running Linux"
-    curl -L https://github.com/grame-cncm/faust/releases/download/$VERSION/libfaust-ubuntu-x86_64.zip -o libfaust-ubuntu-x86_64.zip
-    mkdir -p "ubuntu-x86_64/Release"
-    unzip libfaust-ubuntu-x86_64.zip -d ubuntu-x86_64/Release
+    if [ ! -f libfaust-ubuntu-x86_64.zip ]; then
+        curl -L https://github.com/grame-cncm/faust/releases/download/$VERSION/libfaust-ubuntu-x86_64.zip -o libfaust-ubuntu-x86_64.zip
+        mkdir -p "ubuntu-x86_64/Release"
+        unzip libfaust-ubuntu-x86_64.zip -d ubuntu-x86_64/Release
+    fi
 elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" = "MINGW64_NT" ]; then
     echo "You are running Windows. You should run \"call download_libfaust.bat\"" >&2
     exit 1
